0fc84cf5e5e03c721c7df9ff9bc601ddf40d236c
taylorod <taylorsunwantedemails@gmail.com>
2020-10-29 14:15:50 -0500

    add noteselects component

---
+++ src/components/NoteSelectors.js
@@ -0,0 +1,17 @@
+
+import React, { Component } from 'react'
+import NoteSelector from './NoteSelector'
+
+class NoteSelectors extends Component {
+  render() {
+    return (
+      <div className="note-selectors">
+        <NoteSelector />
+        <NoteSelector />
+        <NoteSelector />
+      </div>
+    )
+  }
+}
+
+export default NoteSelectors
